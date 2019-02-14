bab11396b6700a635ae2ece9d171d3feb9718bd9
hsdyyd <yidong06@sina.com>
2019-02-14 10:04:55 +0800

    处理到达添加页面

--- src/main/java/com/train/springboot/springboot/controller/EmployeeController.java
+++ src/main/java/com/train/springboot/springboot/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.train.springboot.springboot.controller;
 
+import com.train.springboot.springboot.dao.DepartmentDao;
 import com.train.springboot.springboot.dao.EmployeeDao;
+import com.train.springboot.springboot.entities.Department;
 import com.train.springboot.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,11 +20,21 @@ public class EmployeeController {
     @Autowired
     private EmployeeDao employeeDao;
 
+    @Autowired
+    private DepartmentDao departmentDao;
+
     @GetMapping("/emps")
     public String list(Model model){
         Collection<Employee> emps = employeeDao.getAll();
         model.addAttribute("emps",emps);
 
         return "emp/list";
+    }
+
+    @GetMapping("/emp")
+    public String toAdd(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return "emp/add";
     }
 }
